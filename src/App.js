12b086ee2bf7370bc536bed7ef13246529b0e8f1
Your Name <you@example.com>
2021-09-07 21:31:48 +0530

    initial for github

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ❤❤❤Radhy Radhy🧡🧡🧡!
         </p>
         <a
           className="App-link"
